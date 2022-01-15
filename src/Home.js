1be538fc4427354fe4910186e055b40ea26120d6
Andre Alves <andrealvesaraujo.1995@gmail.com>
2022-01-15 17:40:46 -0300

    refactor: Remove other movie images

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,6 @@ import React from "react"
 import "./fonts/B612-Regular.ttf";
 import "./fonts/B612-Bold.ttf";
 import capaLoki from "./img/capaLoki.png";
-import capaRing from "./img/capaRing.png";
-import capaHarry from "./img/capaHarry.jpg";
 import "./img/bgLoki.jpg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClock } from "@fortawesome/free-solid-svg-icons";
@@ -62,8 +60,6 @@ export default class Home extends React.Component {
         <section>
           <aside>
             <img src={capaLoki} alt="Capa do Trailer de Loki"></img>
-            {/* <img src={capaRing} alt="Capa do Trailer do Senhor dos Anéis"></img>
-            <img src={capaHarry} alt="Capa do Trailer do Harry Potter"></img> */}
           </aside>
           <article>
             <p>
